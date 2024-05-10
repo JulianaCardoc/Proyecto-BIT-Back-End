9c0553a0aa5c14cb1d468d020fb8894eeeb0180c
Juliana Cardona <julicardonco@hotmail.com>
2024-05-10 17:31:42 -0500

    merging remote

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1/proyectoBitDB");
-
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
